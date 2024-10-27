74033d4341238ab2899294f4aaed3328ba665fd6
Shahsad KP <shahsadkpklr@gmail.com>
2024-10-27 13:43:23 +0530

    Added versioning setup for rest framework

--- CloudCart/settings.py
+++ CloudCart/settings.py
@@ -153,7 +153,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning'
 }
 
 # Default user model
