4e8d5157e5db80a157dd520f06616c03cbd66b60
Shahsad KP <shahsadkpklr@gmail.com>
2024-10-31 20:07:02 +0530

    Completed urls patterns for store

--- CloudCart/urls_stores.py
+++ CloudCart/urls_stores.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.urls import include, re_path
+
+urlpatterns = [
+    re_path(
+        r'^(?P<version>(v1))/auth/',
+        include('UserAuth.urls'),
+    ),
+    re_path(
+        r'^(?P<version>(v1))/users',
+        include('Users.urls'),
+    ),
+]
